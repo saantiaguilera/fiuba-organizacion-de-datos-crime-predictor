95c2e65f45de1b720c9153afa5ecc25530c79214
toxire <aguilerasanti@hotmail.com>
2015-11-27 00:31:55 -0300

    Commented things

--- KaggleLinierrrrrrs/FileDumper.cpp
+++ KaggleLinierrrrrrs/FileDumper.cpp
@@ -1,24 +1,38 @@
 #include "stdafx.h"
 #include "FileDumper.h"
 
-#define HEADERS "Id,ARSON,ASSAULT,BAD CHECKS,BRIBERY,BURGLARY,DISORDERLY CONDUCT,DRIVING UNDER THE INFLUENCE,DRUG/NARCOTIC,DRUNKENNESS,EMBEZZLEMENT,EXTORTION,FAMILY OFFENSES,FORGERY/COUNTERFEITING,FRAUD,GAMBLING,KIDNAPPING,LARCENY/THEFT,LIQUOR LAWS,LOITERING,MISSING PERSON,NON-CRIMINAL,OTHER OFFENSES,PORNOGRAPHY/OBSCENE MAT,PROSTITUTION,RECOVERED VEHICLE,ROBBERY,RUNAWAY,SECONDARY CODES,SEX OFFENSES FORCIBLE,SEX OFFENSES NON FORCIBLE,STOLEN PROPERTY,SUICIDE,SUSPICIOUS OCC,TREA,TRESPASS,VANDALISM,VEHICLE THEFT,WARRANTS,WEAPON LAWS\n"
+/*
+Using things this way, times are:
+- Writing all 882000 crimes: 607 seconds
+- Flushing a set of 50000 crimes: ~60ms
+- Sending to the buffer a crime prediction: <1ms
+*/
+
+//Dont change
 #define COMMA ","
 #define EOL "\n"
+
+//Change if needed
+#define HEADERS "Id,ARSON,ASSAULT,BAD CHECKS,BRIBERY,BURGLARY,DISORDERLY CONDUCT,DRIVING UNDER THE INFLUENCE,DRUG/NARCOTIC,DRUNKENNESS,EMBEZZLEMENT,EXTORTION,FAMILY OFFENSES,FORGERY/COUNTERFEITING,FRAUD,GAMBLING,KIDNAPPING,LARCENY/THEFT,LIQUOR LAWS,LOITERING,MISSING PERSON,NON-CRIMINAL,OTHER OFFENSES,PORNOGRAPHY/OBSCENE MAT,PROSTITUTION,RECOVERED VEHICLE,ROBBERY,RUNAWAY,SECONDARY CODES,SEX OFFENSES FORCIBLE,SEX OFFENSES NON FORCIBLE,STOLEN PROPERTY,SUICIDE,SUSPICIOUS OCC,TREA,TRESPASS,VANDALISM,VEHICLE THEFT,WARRANTS,WEAPON LAWS\n"
 #define MAX_CRIMES_IN_BUFFER 50000
 
+//Dont change
 FileDumper::FileDumper() : count(0) {
 	dumpFile.open("dump.csv", std::ofstream::out | std::ofstream::trunc);
 	dumpHeaders();
 }
 
+//Dont change
 FileDumper::~FileDumper() {
 	dumpFile.close();
 }
 
+//Dont change
 void FileDumper::dumpHeaders() {
 	dumpFile << HEADERS;
 }
 
+//Dont change
 void FileDumper::dumpPrediction(std::string& id, std::map<std::string, long double>& pointsMap, long double& totalPoints) {
 	if (count >= MAX_CRIMES_IN_BUFFER) {
 		flushBuffer();
@@ -28,6 +42,7 @@ void FileDumper::dumpPrediction(std::string& id, std::map<std::string, long doub
 	count++;
 }
 
+//Dont change
 void FileDumper::flushBuffer() {
 	dumpFile << buffer.str();
 	buffer.clear();
@@ -35,6 +50,8 @@ void FileDumper::flushBuffer() {
 	count = 0;
 }
 
+//On each '0' goes the percentage for each category. See the HEADERS define to know the order. 
+//Dont change the structure or the COMMA / << / buffer / EOL things. Its optimized this way
 void FileDumper::addToBuffer(std::string& id, std::map<std::string, long double>& pointsMap, long double& totalPoints) {
 	buffer << id
 		<< COMMA
--- KaggleLinierrrrrrs/FileDumper.h
+++ KaggleLinierrrrrrs/FileDumper.h
@@ -5,14 +5,22 @@
 
 class FileDumper {
 private:
+	//Output file
 	std::ofstream dumpFile;
+	//Buffer to store the data temporaly
 	std::ostringstream buffer;
+	//For flushing when it reaches a certain amount of predictions
 	int count;
+	//Dumps the initial headers (see define in .cpp)
 	void dumpHeaders();
+	//Should transform accordingly to the data need to store. Contains the algorythm that makes the percentages for each category
 	void addToBuffer(std::string& id, std::map<std::string, long double>& pointsMap, long double& totalPoints);
 public:
+	//Initializers
 	FileDumper();
 	~FileDumper();
+	//Should transform accordingly to the data need to store. Algorythm for each category is implemented in addToBuffer not here
 	void dumpPrediction(std::string& id, std::map<std::string, long double>& pointsMap, long double& totalPoints);
+	//Flushes the buffer contents in the file
 	void flushBuffer();
 };
